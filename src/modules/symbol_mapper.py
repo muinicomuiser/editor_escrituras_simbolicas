5e0c3abff42431f7b12037bb804560dfb998106d
muinicomuiser <nicodoneg@gmail.com>
2026-07-27 16:41:55 -0400

    Limpieza Symbol Mapper

--- src/modules/symbol_mapper.py
+++ src/modules/symbol_mapper.py
@@ -16,26 +16,6 @@ class SymbolMapper:
     def has_image(self, character: str) -> bool:
         return character in self._character_map     
 
-
-
-    # ## 1. Bloque repetido
-    # def load_default_set(self):
-    #     self._character_map.clear()
-    #     self._current_dir
-
-    #     if not os.path.exists(self._current_dir) or not os.path.isdir(self._current_dir):
-    #         return
-
-    #     for entry in os.scandir(self._current_dir):
-    #         if entry.is_file():
-    #             filename, ext = os.path.splitext(entry.name)
-    #             if ext.lower() in self._valid_extensions:
-    #                 if len(filename) == 1:
-    #                     char_key = filename[0]
-    #                     self._character_map[char_key] = entry.path
-    #                 elif filename.lower() == "space":
-    #                     self._character_map[" "] = entry.path
-
     def load_from_directory(self, path: str):
         self._character_map.clear()
         self._current_dir = path
@@ -53,4 +33,3 @@ class SymbolMapper:
                     elif filename.lower() == "space": 
                         self._character_map[" "] = entry.path
 
-    ## 1. Fin Bloque repetido
